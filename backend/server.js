e8a5ce120e73ddeac4e30c34858083415d355ccd
Cole Saldanha <121999809+elcosas@users.noreply.github.com>
2025-10-04 21:33:40 -0700

    Add GET and POST support to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,34 @@
 import http from 'http';
 
+const PORT = 8080;
+
+const CONTENT_TYPE_JSON = { "Content-Type": "application/json" };
+
+let dummy_data = [
+  {code: 'cole', tasks: ['do laundry', 'buy milk']},
+  {code: 'sean', tasks: ['clean dishes']}
+];
+
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, World!\n');
+  const {method, url} = req;
+  if (method == 'GET') {
+    console.log('handling a GET request...');
+    res.writeHead(200, CONTENT_TYPE_JSON);
+    res.end(JSON.stringify(dummy_data));
+  }
+  else if (method == 'POST') {
+    console.log('handling a POST request...');
+    res.writeHead(200, CONTENT_TYPE_JSON);
+    res.end(JSON.stringify(dummy_data));
+  }
+  else {
+    res.writeHead(404, CONTENT_TYPE_JSON);
+    res.end(JSON.stringify({error: 'Not Found'}));
+  }
 });
 
-server.listen(8080, 'localhost', () => {
-  console.log('Server running @ http://localhost:8080/');
+server.listen(PORT, 'localhost', () => {
+  console.log(`Server running @ http://localhost:${PORT}/`);
 });
+
+

019d97884ad22d256fcf554781bd3637dc132bb9
Cole Saldanha <121999809+elcosas@users.noreply.github.com>
2025-10-04 22:06:48 -0700

    Add basic GET & POST routing for / and /api/ in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import http from 'http';
+import fs from 'fs';
 
 const PORT = 8080;
 
-const CONTENT_TYPE_JSON = { "Content-Type": "application/json" };
+const CONTENT_TYPE_JSON = {"Content-Type": "application/json"};
+const CONTENT_TYPE_HTML = {"Content-Type": "text/html"}; 
 
 let dummy_data = [
   {code: 'cole', tasks: ['do laundry', 'buy milk']},
@@ -11,15 +13,18 @@ let dummy_data = [
 
 const server = http.createServer((req, res) => {
   const {method, url} = req;
-  if (method == 'GET') {
-    console.log('handling a GET request...');
+  if (method == 'GET' && url.includes('/api/')) {
     res.writeHead(200, CONTENT_TYPE_JSON);
     res.end(JSON.stringify(dummy_data));
   }
-  else if (method == 'POST') {
-    console.log('handling a POST request...');
+  else if (method == 'POST' && url.includes('/api/')) {
     res.writeHead(200, CONTENT_TYPE_JSON);
     res.end(JSON.stringify(dummy_data));
+  }
+  else if (method == 'GET') {
+    const html = fs.readFileSync('./frontend/index.html', 'utf8');
+    res.writeHead(200, CONTENT_TYPE_JSON);
+    res.end(html);
   }
   else {
     res.writeHead(404, CONTENT_TYPE_JSON);
@@ -30,5 +35,3 @@ const server = http.createServer((req, res) => {
 server.listen(PORT, 'localhost', () => {
   console.log(`Server running @ http://localhost:${PORT}/`);
 });
-
-

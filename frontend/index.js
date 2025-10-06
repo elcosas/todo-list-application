ce090d604bc695ffe694a763b436f7f765cfab42
Sean San <seanksan@gmail.com>
2025-10-05 18:21:33 -0700

    Fix the add item btn not updating to text box bug
    The add item button could only update to a text box once. By moving
    around the query selecting the buttons now turns into a text box and
    back how ever many times the user needs it to.

--- frontend/index.js
+++ frontend/index.js
@@ -1,22 +1,24 @@
 // Query Selectors
 const q_todoListItemArea = document.querySelector("#todo-list-items-area");
-const q_btnAddToDoList = document.querySelector("#todo-list-add-item-button");
 
 // Event listners
-q_btnAddToDoList.addEventListener("click", () => {
-    const inputBox = document.createElement("input");
-    inputBox.setAttribute("style", "color: blue; background: white;");
-    q_todoListItemArea.replaceChild(inputBox, q_btnAddToDoList);
+q_todoListItemArea.addEventListener("click", (event) => {
+    const q_btnAddToDoList = document.querySelector("#todo-list-add-item-button");
+    if(event.target.id === "todo-list-add-item-button") {
+        const inputBox = document.createElement("input");
+        inputBox.setAttribute("style", "color: blue; background: white;");
+        q_todoListItemArea.replaceChild(inputBox, q_btnAddToDoList);
 
-    inputBox.addEventListener("keydown", function(event) {
-        if(event.key == "Enter") {
-            alert("Hey");
-            const addItemButton = document.createElement("button");
-            addItemButton.id = "todo-list-add-item-button";
-            addItemButton.textContent = "+ Add Item";
-            q_todoListItemArea.replaceChild(addItemButton, inputBox);
-        }
-});
+        inputBox.addEventListener("keydown", function(event) {
+            if(event.key == "Enter") {
+                const addItemButton = document.createElement("button");
+                addItemButton.id = "todo-list-add-item-button";
+                addItemButton.textContent = "+ Add Item";
+                q_todoListItemArea.replaceChild(addItemButton, inputBox);
+            }
+        });
+    }
+
 });
 
 
